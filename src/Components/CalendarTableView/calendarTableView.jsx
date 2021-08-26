93e0a7edf22433a17270e5c4a70963849963626c
CyrusWarner <cyruswarner27@gmail.com>
2021-08-26 10:56:38 -0500

    Added a table view for all events on the calendar component.

---
+++ src/Components/CalendarTableView/calendarTableView.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container, Row, Table, Col } from "react-bootstrap";
+const CalendarTableView = ({ events }) => {
+  return (
+    <Container fluid>
+      <Row>
+          <Col sm={1}></Col>
+        <Col sm={10}>
+            <div>
+          <Table  responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th>Event Title</th>
+                <th>Event Assignee</th>
+                <th>Event Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {events.map((event) => {
+                  return(
+                <tr>
+                  <td>{event.title}</td>
+                  <td>{event.assignee}</td>
+                  <td>{event.date}</td>
+                </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          </div>
+        </Col>
+        <Col sm={1}></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CalendarTableView;
