9474a5cb07936217340e93fe04c77b1f8a7dc879
CyrusWarner <cyruswarner27@gmail.com>
2021-09-03 18:21:39 -0500

    Added a display for what a users role is to the invite coworkers section.

--- src/Components/ShowCoworkers/showCoworkers.jsx
+++ src/Components/ShowCoworkers/showCoworkers.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {Card, Container, Row } from "react-bootstrap";
 import * as AiIcons from 'react-icons/ai'
+import './showCoworker.css'
 
 const ShowCoworkers = ({ boardUsers, removeUser, currentUser }) => {
   const [search, setSearch] = useState("");
@@ -21,7 +22,9 @@ const ShowCoworkers = ({ boardUsers, removeUser, currentUser }) => {
           {userId !== userData.userId &&
             <div className="position-absolute top-50 end-0 translate-middle-y"><AiIcons.AiOutlineClose  style={{cursor: "pointer "}} onClick={() => (removeUser(userData.userId))} color="red" size="1.5rem" /></div>
             }
-            <Card.Body><div className="fs-5 ">{userData.user.email}</div>
+            <Card.Body>
+          <h3 className="coworkerRole" >{userData.roles.roleName}</h3>
+              <div className="fs-5 ">{userData.user.email}</div>
             </Card.Body>
           </Card>
           </div>
