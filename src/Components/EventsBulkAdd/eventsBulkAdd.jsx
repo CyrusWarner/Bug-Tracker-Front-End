aa3013b19a9b1f26057da1a723c0e79f624abdc2
Cyrus Warner <cyruswarner27@gmail.com>
2021-09-13 12:38:43 -0800

    Added conditional to check the type of file a user uploads for events bulk add.

--- src/Components/EventsBulkAdd/eventsBulkAdd.jsx
+++ src/Components/EventsBulkAdd/eventsBulkAdd.jsx
@@ -1,20 +1,21 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Col, Row } from "react-bootstrap";
 import { toast } from "react-toastify";
 
 const EventsBulkAdd = ({ currentBoard, getAllEvents }) => {
   const { boardId } = currentBoard;
   const [csvFile, setCsvFile] = useState();
+  console.log(csvFile)
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("file", csvFile);
-    await axios
+    formData.append("inputFile", csvFile);
+    if(csvFile.type === "application/vnd.ms-excel"){
+      await axios
       .post(
         `http://localhost:27029/api/Events/uploadfile/csv/${boardId}`,
-        formData,
-        { headers: { "content-type": "multipart/form-data" } }
+        formData
       )
       .then((res) => {
         if (res.status === 200) {
@@ -25,22 +26,46 @@ const EventsBulkAdd = ({ currentBoard, getAllEvents }) => {
       })
       .catch((err) => {
         if (err) {
-          toast.error("Error uploading csv file")
+          toast.error("Error uploading csv file");
+          e.target.reset();
         }
       });
+    }
+    else {
+      toast.error("Please Upload a file with the .csv extension");
+      e.target.reset();
+    }
+
   };
   return (
     <div>
       <Form onSubmit={handleSubmit}>
-        <Button type="submit" className="me-2">
-          Bulk Add Events
-        </Button>
-        <input
-          type="file"
-          accept=".csv"
-          id="csvfile"
-          onChange={(e) => setCsvFile(e.target.files[0])}
-        ></input>
+        <Container className="g-0">
+          <Row>
+            <Col sm={4}>
+              <input
+                className="form-control"
+                type="file"
+                accept=".csv"
+                id="csvfile"
+                onChange={(e) => setCsvFile(e.target.files[0])}
+              ></input>
+            </Col>
+            <Col sm={4}></Col>
+            <Col sm={4}></Col>
+          </Row>
+        </Container>
+        <Container className="g-0 mt-3">
+          <Row>
+            <Col sm={4}>
+              <Button type="submit" className="me-2">
+                Bulk Add Events (.csv)
+              </Button>
+            </Col>
+            <Col sm={4}> </Col>
+            <Col sm={4}></Col>
+          </Row>
+        </Container>
       </Form>
     </div>
   );
