0b1553633cb437018b9e5345758f8aa746d4f8cc
Cyrus Warner <cyruswarner27@gmail.com>
2021-09-13 12:47:59 -0800

    Adjusted positiong of calendar to be in a two column grid layout.

--- src/Components/Calendar/calendar.jsx
+++ src/Components/Calendar/calendar.jsx
@@ -7,12 +7,17 @@ import NewEventModal from "../NewEventModal/newEventModal";
 import CalendarTableView from "../CalendarTableView/calendarTableView";
 import EventsBulkAdd from "../EventsBulkAdd/eventsBulkAdd";
 import * as AiIcons from "react-icons/ai";
-import * as BsIcons from "react-icons/bs"
+import * as BsIcons from "react-icons/bs";
 import "./calendar.css";
 import axios from "axios";
 import { toast } from "react-toastify";
 import CalendarEventDetails from "../CalendarEventDetails/calendarEventDetails";
-const Calendar = ({ currentBoard, userRole, boardUsers, displayBoardUsers }) => {
+const Calendar = ({
+  currentBoard,
+  userRole,
+  boardUsers,
+  displayBoardUsers,
+}) => {
   const [show, setShow] = useState(false);
   const [tableView, setTableView] = useState(false);
   const handleClose = () => setShow(false);
@@ -44,7 +49,7 @@ const Calendar = ({ currentBoard, userRole, boardUsers, displayBoardUsers }) => 
         if (res.status === 200) {
           setEvents(res.data);
         }
-      })
+      });
   };
   useEffect(() => {
     getAllEvents();
@@ -52,11 +57,10 @@ const Calendar = ({ currentBoard, userRole, boardUsers, displayBoardUsers }) => 
 
   const handleDateClick = (dateClick) => {
     const filterEvents = events.filter((event) => {
-      if(event.date === dateClick.dateStr){
+      if (event.date === dateClick.dateStr) {
         return event;
       }
-    }
-    );
+    });
     setFilteredEvents(filterEvents);
     handleShow();
   };
@@ -68,51 +72,88 @@ const Calendar = ({ currentBoard, userRole, boardUsers, displayBoardUsers }) => 
     let month = dateObj.getMonth() + 1;
     let date = dateObj.getDate();
     let paddedMonth = month.toString();
-    if(paddedMonth.length < 2)
-    {
+    if (paddedMonth.length < 2) {
       paddedMonth = "0" + paddedMonth;
     }
     let paddedDate = date.toString();
-    if(paddedDate.length < 2)
-    {
+    if (paddedDate.length < 2) {
       paddedDate = "0" + paddedDate;
     }
-    let dateToStore = `${year}-${paddedMonth}-${paddedDate}`
-    events.forEach(event => {
-      if(event.eventsId === id)
-      {
+    let dateToStore = `${year}-${paddedMonth}-${paddedDate}`;
+    events.forEach((event) => {
+      if (event.eventsId === id) {
         event.date = dateToStore;
-        axios.patch(`http://localhost:27029/api/Events/`, event)
+        axios.patch(`http://localhost:27029/api/Events/`, event);
       }
-    })
-  }
-  const bulkAdd = () => {
-
-  }
+    });
+  };
+  const bulkAdd = () => {};
   return (
     <React.Fragment>
       <Container>
         <Row>
-          <Col sm={1}></Col>
-          <Col sm={10}>
-            <h1 className="title">Employee Calendar<AiIcons.AiOutlineCalendar color="#45A29E" className="ms-1" size="3rem" /></h1>
-            <p className="font-description fs-5">Click On Calendar Day For The Day's Events</p>
-            {tableView 
-              ? (<div style={{color: "#45A29E"}} className="fs-4">Show Calendar View <AiIcons.AiOutlineCalendar size="2rem" onClick={() => setTableView(!tableView)} style={{cursor: "pointer", color: "#45A29E"}}/></div>)
-              : (<div style={{color: "#45A29E"}} className="fs-4">Show Table View <BsIcons.BsTable  onClick={() => setTableView(!tableView)} style={{cursor: "pointer", color: "#45A29E"}}/></div>)
-            }
-   
+          <Col sm={4}>
+            <h1 className="title">
+              Employee Calendar
+              <AiIcons.AiOutlineCalendar
+                color="#45A29E"
+                className="ms-1"
+                size="3rem"
+              />
+            </h1>
+            <p className="font-description fs-5">
+              Click On Calendar Day For The Day's Events
+            </p>
+            {tableView ? (
+              <div style={{ color: "#45A29E" }} className="fs-4">
+                Show Calendar View{" "}
+                <AiIcons.AiOutlineCalendar
+                  size="2rem"
+                  onClick={() => setTableView(!tableView)}
+                  style={{ cursor: "pointer", color: "#45A29E" }}
+                />
+              </div>
+            ) : (
+              <div style={{ color: "#45A29E" }} className="fs-4">
+                Show Table View{" "}
+                <BsIcons.BsTable
+                  onClick={() => setTableView(!tableView)}
+                  style={{ cursor: "pointer", color: "#45A29E" }}
+                />
+              </div>
+            )}
+
             <div>
               {(userRole === "Admin" || userRole === "Board Owner") && (
                 <div data-testid="newEventModal">
-                <NewEventModal onEventAdded={onEventAdded} boardUsers={boardUsers} currentBoard={currentBoard} displayBoardUsers={displayBoardUsers}/>
-                <EventsBulkAdd currentBoard={currentBoard} getAllEvents={getAllEvents}/>
+                  <NewEventModal
+                    onEventAdded={onEventAdded}
+                    boardUsers={boardUsers}
+                    currentBoard={currentBoard}
+                    displayBoardUsers={displayBoardUsers}
+                  />
+                  <EventsBulkAdd
+                    currentBoard={currentBoard}
+                    getAllEvents={getAllEvents}
+                  />
                 </div>
               )}
             </div>
           </Col>
-          <Col sm={1}>
-          <CalendarEventDetails filteredEvents={filteredEvents} show={show} handleClose={handleClose} getAllEvents={getAllEvents} userRole={userRole}/>
+          <Col sm={8}>
+          <div className="m-3 text-center">
+              {!tableView && (
+                <FullCalendar
+                  plugins={[dayGridPlugin, interactionPlugin]}
+                  dateClick={handleDateClick}
+                  initialView="dayGridMonth"
+                  weekends={false}
+                  events={events}
+                  editable={true}
+                  eventDrop={(info) => calendarEventDrop(info.event)}
+                ></FullCalendar>
+              )}
+            </div>
           </Col>
         </Row>
       </Container>
@@ -129,26 +170,18 @@ const Calendar = ({ currentBoard, userRole, boardUsers, displayBoardUsers }) => 
         <Row>
           <Col sm={1}></Col>
           <Col sm={10}>
-            <div className="m-3 text-center">
-            {!tableView &&
-                          <FullCalendar
-                          plugins={[dayGridPlugin, interactionPlugin]}
-                          dateClick={handleDateClick}
-                          initialView="dayGridMonth"
-                          weekends={false}
-                          events={events}
-                          editable={true}
-                          eventDrop= {(info) => calendarEventDrop(info.event)}
-                        ></FullCalendar>
-            }
-            </div>
           </Col>
           <Col sm={1}></Col>
         </Row>
       </Container>
-      {tableView &&
-            <CalendarTableView events={events}/>
-            }
+      {tableView && <CalendarTableView events={events} />}
+      <CalendarEventDetails
+        filteredEvents={filteredEvents}
+        show={show}
+        handleClose={handleClose}
+        getAllEvents={getAllEvents}
+        userRole={userRole}
+      />
     </React.Fragment>
   );
 };
--- src/Components/EventsBulkAdd/eventsBulkAdd.jsx
+++ src/Components/EventsBulkAdd/eventsBulkAdd.jsx
@@ -6,66 +6,48 @@ import { toast } from "react-toastify";
 const EventsBulkAdd = ({ currentBoard, getAllEvents }) => {
   const { boardId } = currentBoard;
   const [csvFile, setCsvFile] = useState();
-  console.log(csvFile)
+  console.log(csvFile);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("inputFile", csvFile);
-    if(csvFile.type === "application/vnd.ms-excel"){
+    if (csvFile.type === "application/vnd.ms-excel") {
       await axios
-      .post(
-        `http://localhost:27029/api/Events/uploadfile/csv/${boardId}`,
-        formData
-      )
-      .then((res) => {
-        if (res.status === 200) {
-          toast.success("Csv Uploaded");
-          getAllEvents();
-          e.target.reset();
-        }
-      })
-      .catch((err) => {
-        if (err) {
-          toast.error("Error uploading csv file");
-          e.target.reset();
-        }
-      });
-    }
-    else {
+        .post(
+          `http://localhost:27029/api/Events/uploadfile/csv/${boardId}`,
+          formData
+        )
+        .then((res) => {
+          if (res.status === 200) {
+            toast.success("Csv Uploaded");
+            getAllEvents();
+            e.target.reset();
+          }
+        })
+        .catch((err) => {
+          if (err) {
+            toast.error("Error uploading csv file");
+            e.target.reset();
+          }
+        });
+    } else {
       toast.error("Please Upload a file with the .csv extension");
       e.target.reset();
     }
-
   };
   return (
     <div>
       <Form onSubmit={handleSubmit}>
-        <Container className="g-0">
-          <Row>
-            <Col sm={4}>
-              <input
-                className="form-control"
-                type="file"
-                accept=".csv"
-                id="csvfile"
-                onChange={(e) => setCsvFile(e.target.files[0])}
-              ></input>
-            </Col>
-            <Col sm={4}></Col>
-            <Col sm={4}></Col>
-          </Row>
-        </Container>
-        <Container className="g-0 mt-3">
-          <Row>
-            <Col sm={4}>
-              <Button type="submit" className="me-2">
-                Bulk Add Events (.csv)
-              </Button>
-            </Col>
-            <Col sm={4}> </Col>
-            <Col sm={4}></Col>
-          </Row>
-        </Container>
+        <input
+          className="form-control"
+          type="file"
+          accept=".csv"
+          id="csvfile"
+          onChange={(e) => setCsvFile(e.target.files[0])}
+        ></input>
+        <Button type="submit" className="me-2 mt-3">
+          Bulk Add Events (.csv)
+        </Button>
       </Form>
     </div>
   );
