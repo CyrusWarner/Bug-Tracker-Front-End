df2e214e58237bc54139cc64320ce520d66e3499
CyrusWarner <cyruswarner27@gmail.com>
2021-09-19 12:24:37 -0500

    Added functionality for copying a users email in emailContactForm.

--- src/Components/EmailContactForm/emailContactForm.jsx
+++ src/Components/EmailContactForm/emailContactForm.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Row, Col, Card } from "react-bootstrap";
 import emailjs from "emailjs-com";
 import { toast } from "react-toastify";
-
+import * as AiIcons from "react-icons/ai";
+import CopyToClipboard from "react-copy-to-clipboard";
 const EmailContactForm = ({ currentUser, boardUsers }) => {
+  const [copied, setCopied] = useState(false);
   const { email } = currentUser;
   const sendEmail = (e) => {
     e.preventDefault();
@@ -42,13 +44,25 @@ const EmailContactForm = ({ currentUser, boardUsers }) => {
                   <label className="floatingInput">From Email:</label>
                 </div>
                 <div className="form-floating mt-2">
-                <input name="to_email" className="form-control" placeholder="Enter Board Users Email..."></input>
-                <label className="floatingInput">Enter Board Users Email...</label>
+                  <input
+                    name="to_email"
+                    className="form-control"
+                    placeholder="Enter Board Users Email..."
+                  ></input>
+                  <label className="floatingInput">
+                    Enter Board Users Email...
+                  </label>
                 </div>
               </div>
               <div className="form-floating mt-2">
-                <input name="message" className="form-control" placeholder="Enter A Message For Your Email..."></input>
-                <label className="floatingInput">Enter A Message For Your Email...</label>
+                <input
+                  name="message"
+                  className="form-control"
+                  placeholder="Enter A Message For Your Email..."
+                ></input>
+                <label className="floatingInput">
+                  Enter A Message For Your Email...
+                </label>
               </div>
               <Button className="mt-2" type="submit">
                 Send Email
@@ -56,18 +70,27 @@ const EmailContactForm = ({ currentUser, boardUsers }) => {
             </form>
           </Col>
           <Col sm={6}>
-              <div className="wrapper">
+            <div className="wrapper">
               {boardUsers.map((boardUser) => {
-                  return (
-                    <div key={boardUser.userId} className="item">
-                    <Card className="cardContainer mt-2 mb-2"> 
-                      <Card.Body><div className="fs-5">{boardUser.user.email}</div>
+                return (
+                  <div key={boardUser.userId} className="item">
+                    <Card className="cardContainer mt-2 mb-2">
+                      <Card.Body className="d-flex justify-content-between">
+                        <div className="fs-5">{boardUser.user.email}</div>
+                        <CopyToClipboard
+                          text={boardUser.user.email}
+                          onCopy={() => {
+                            setCopied(true);
+                          }}
+                        >
+                          <AiIcons.AiFillCopy  size="1.5rem" />
+                        </CopyToClipboard>
                       </Card.Body>
                     </Card>
-                    </div>
-                  )
+                  </div>
+                );
               })}
-              </div>
+            </div>
           </Col>
         </Row>
       </Container>
